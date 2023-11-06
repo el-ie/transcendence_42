69c970a74b4df13757aa2b7188d2b1df967faa94
Elie AMAR <eamar@made-f0Br8s10.clusters.42paris.fr>
2023-11-06 12:04:32 +0100

    create fake user

--- backend/src/forTest/forTest.controller.ts
+++ backend/src/forTest/forTest.controller.ts
@@ -1,5 +1,5 @@
 import { ForTestService } from 'src/forTest/forTest.service';
-import { Controller, Get, Req, UseGuards, Res, HttpStatus, HttpException, Post, Body, HttpCode } from "@nestjs/common";
+import { Controller, Get, Req, UseGuards, Res, HttpStatus, HttpException, Post, Body, HttpCode, Query } from "@nestjs/common";
 import { AuthService } from "src/auth/auth.service";
 import { AuthGuard } from '@nestjs/passport';
 import { Public } from "src/decorators/public.decorator";
@@ -9,30 +9,43 @@ import { PrismaService } from "src/prisma/prisma.service";
 import { AvoidTwoFa } from "src/decorators/avoidtwofa.decorator";
 
 
+@Public()
 @Controller('test')
 export class ForTestController {
-  constructor(private authService: AuthService, private readonly forTestService: ForTestService, private prisma: PrismaService) {}
+	constructor(private authService: AuthService, private readonly forTestService: ForTestService, private prisma: PrismaService) {}
 
-  @Public()
-  @Post('create_fake_user')
-  async fake_user(@Body() body) {
+	@Public()
+	@Get('create_fake_user')
+	async fake_user(@Body() body, @Req() req, @Res() response, @Query('username') username: string, @Query('login') login: string) {
 
-	  let user = await this.prisma.user.findUnique( { where: { username: body.username } });
-	  if (user)
-		  return 'username already taken';
-	  user = await this.prisma.user.findUnique( { where: { login: body.login } });
-	  if (user)
-		  return 'login already taken';
+		let user = await this.prisma.user.findUnique( { where: { username: username } });
+		if (user)
+			response.send('username already taken');
+		user = await this.prisma.user.findUnique( { where: { login: login } });
+		if (user)
+			response.send('login already taken');
 
 		user = await this.prisma.user.create({
 			data: {
-				login: body.login,
-				username: body.username,
+				login: login,
+				username: username,
 				sessionId: "no need",
 			},
 		});
 
-		return  (await this.prisma.user.findMany());
-		//return 'succes';
-  }
+		const token = await this.authService.generateJwt(user, 'basic_auth');
+
+		response.cookie('AUTH_TOKEN', token, { httpOnly: false });
+
+		//return response.send();//options?
+		response.redirect('http://localhost:3000/home');
+			//return  (await this.prisma.user.findMany());
+	}
+
+	@Public()
+	@Get('list_users')
+	async list_users()
+	{
+			return  (await this.prisma.user.findMany());
+	}
 }
